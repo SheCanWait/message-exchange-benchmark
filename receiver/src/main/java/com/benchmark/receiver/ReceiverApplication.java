d46825bdfe6743fb3bfc29ec4057b259a0496fa9
daniel-krol <krolu98@gmail.com>
2021-11-14 21:51:11 +0100

    configure rabbitmq in sender and receiver, successfully sent and received a message via rabbit

--- receiver/src/main/java/com/benchmark/receiver/ReceiverApplication.java
+++ receiver/src/main/java/com/benchmark/receiver/ReceiverApplication.java
@@ -1,13 +1,45 @@
 package com.benchmark.receiver;
 
+import com.rabbitmq.client.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
 
 @SpringBootApplication
 public class ReceiverApplication {
 
-	public static void main(String[] args) {
+	static Logger logger = LoggerFactory.getLogger(ReceiverApplication.class);
+
+	private final static String QUEUE_NAME = "hello";
+
+	public static void main(String[] args) throws IOException, TimeoutException {
 		SpringApplication.run(ReceiverApplication.class, args);
+
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		channel.queueDeclare(QUEUE_NAME,
+				false, false,false, null);
+		logger.info("[!] Waiting for messages. To exit press Ctrl+C");
+
+		Consumer consumer = new DefaultConsumer(channel){
+			@Override
+			public void handleDelivery(String consumerTag,
+									   Envelope envelope,
+									   AMQP.BasicProperties properties,
+									   byte[] body)
+					throws IOException {
+				String message = new String(body, StandardCharsets.UTF_8);
+				logger.info("[x] Message Recieved' " + message + "'");
+			}
+		};
+		channel.basicConsume(QUEUE_NAME, true, consumer);
 	}
 
 }
--- sender/src/main/java/com/benchmark/sender/SenderApplication.java
+++ sender/src/main/java/com/benchmark/sender/SenderApplication.java
@@ -1,13 +1,43 @@
 package com.benchmark.sender;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
 
 @SpringBootApplication
 public class SenderApplication {
 
-	public static void main(String[] args) {
+	private final static String QUEUE_NAME = "hello";
+	static Logger logger
+			= LoggerFactory.getLogger(SenderApplication.class);
+
+	public static void main(String[] args) throws IOException, TimeoutException {
 		SpringApplication.run(SenderApplication.class, args);
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		channel.queueDeclare(QUEUE_NAME,
+				false,
+				false,
+				false,
+				null);
+		String message = "Welcome to RabbitMQ 33";
+		channel.basicPublish("",
+				QUEUE_NAME,
+				null,
+				message.getBytes(StandardCharsets.UTF_8));
+		logger.debug("[!] Sent '" + message + "'");
+		channel.close();
+		connection.close();
 	}
 
 }
