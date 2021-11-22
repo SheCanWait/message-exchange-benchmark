7a320c6e2ac0ffc2c10a270f92068cfe85ff27f3
daniel-krol <krolu98@gmail.com>
2021-11-22 20:35:35 +0100

    Kafka sender and receiver starts, but receiver looks for Kafka server on localhost:9092 instead of 29092

--- receiver/src/main/java/com/benchmark/receiver/ReceiverApplication.java
+++ receiver/src/main/java/com/benchmark/receiver/ReceiverApplication.java
@@ -1,24 +1,80 @@
 package com.benchmark.receiver;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.context.IntegrationFlowContext;
+import org.springframework.integration.kafka.dsl.Kafka;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.PollableChannel;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
+@Slf4j
 @SpringBootApplication
 public class ReceiverApplication {
 
+	@Autowired
+	PollableChannel consumerChannel;
+
 	private static final RabbitReceiver rabbitReceiver = new RabbitReceiver();
-	private static final KafkaReceiver kafkaReceiver = new KafkaReceiver();
 
 	public static void main(String[] args) throws IOException, TimeoutException {
 		SpringApplication.run(ReceiverApplication.class, args);
+
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(ReceiverApplication.class).web(WebApplicationType.NONE).run(args);
+		List<String> topics = Arrays.asList("FANTASY", "HORROR", "ROMANCE", "THRILLER");
+		context.getBean(ReceiverApplication.class).run(context, topics);
+		context.close();
 
 //		rabbitReceiver.startReceivingRabbitMessages();
 
 		//kafka listener listens by default
 
+	}
+
+	private void run(ConfigurableApplicationContext context, List<String> topics) {
+		log.info("Inside ProducerApplication run method...");
+		PollableChannel consumerChannel = context.getBean("consumerChannel", PollableChannel.class);
+
+		for (String topic : topics)
+			addAnotherListenerForTopics(topic);
+
+		Message<?> received = consumerChannel.receive();
+		while (received != null) {
+			received = consumerChannel.receive();
+			//System.out.println("Received " + received.getPayload());
+			log.info("Received" + received.getPayload());
+		}
+	}
+
+	@Autowired
+	private IntegrationFlowContext flowContext;
+
+	@Autowired
+	private KafkaProperties kafkaProperties;
+
+	public void addAnotherListenerForTopics(String... topics) {
+		Map<String, Object> consumerProperties = kafkaProperties.buildConsumerProperties();
+		consumerProperties.put("group.id", "dummy");
+		IntegrationFlow flow = IntegrationFlows
+				.from(Kafka.messageDrivenChannelAdapter(
+						new DefaultKafkaConsumerFactory<String, String>(consumerProperties), topics))
+				.channel("consumerChannel").get();
+		this.flowContext.registration(flow).register();
 	}
 
 }
--- sender/src/main/java/com/benchmark/sender/SenderApplication.java
+++ sender/src/main/java/com/benchmark/sender/SenderApplication.java
@@ -1,28 +1,56 @@
 package com.benchmark.sender;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.GenericMessage;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
+@Slf4j
 @SpringBootApplication
 public class SenderApplication {
 
 	private static final RabbitSender rabbitSender = new RabbitSender();
-	private static final KafkaSender kafkaSender = new KafkaSender();
+//	private static final KafkaSender kafkaSender = new KafkaSender();
 
 	public static void main(String[] args) {
 		SpringApplication.run(SenderApplication.class, args);
 
+//		ApplicationContext context = new AnnotationConfigApplicationContext(ProducerChannelConfig.class);
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(SenderApplication.class).web(WebApplicationType.NONE).run(args);
+		context.getBean(SenderApplication.class).run(context);
+		context.close();
+
 //		rabbitSender.executeRabbitTests(MessagePreparator.prepareMessage(100000000), 100, true);
 //		rabbitSender.executeRabbitTests(MessagePreparator.prepareMessage(100000000), 100, false);
 
-		kafkaSender.executeKafkaTests("dupa1", 5);
+//		kafkaSender.executeKafkaTests("dupa1", 5);
 
+	}
+
+	private void run(ConfigurableApplicationContext context) {
+		log.info("Inside ProducerApplication run method...");
+		MessageChannel producerChannel = context.getBean("producerChannel", MessageChannel.class);
+
+
+		Map<String, Object> headers = Collections.singletonMap(KafkaHeaders.TOPIC, "test-topic");
+		producerChannel.send(new GenericMessage<>("dupa1234", headers));
+
+		log.info("Finished ProducerApplication run method...");
 	}
 
 }
